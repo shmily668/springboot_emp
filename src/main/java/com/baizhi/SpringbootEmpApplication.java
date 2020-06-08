16ce471fe54234a6609e08a6176c6eefdbc22634
shmily668 <66618067+shmily668@users.noreply.github.com>
2020-06-08 21:26:49 +0800

    Update SpringbootEmpApplication.java

--- src/main/java/com/baizhi/SpringbootEmpApplication.java
+++ src/main/java/com/baizhi/SpringbootEmpApplication.java
@@ -10,6 +10,7 @@ public class SpringbootEmpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootEmpApplication.class, args);
+        System.out.print("I am super boy!");
     }
 
 }
